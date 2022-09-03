e82b2b19ecc6505e96171cca80c73c70dfe67980
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-03 14:59:40 +0300

    updated CSS and JS

--- game.js
+++ game.js
@@ -1,31 +1,36 @@
 var start = document.getElementById('start');
-
 const boundary1pos = document.getElementById("boundary1").getBoundingClientRect()
 const boundary2pos = document.getElementById("boundary2").getBoundingClientRect()
 const boundary3pos = document.getElementById("boundary3").getBoundingClientRect()
 const boundary4pos = document.getElementById("boundary4").getBoundingClientRect()
 const boundary5pos = document.getElementById("boundary5").getBoundingClientRect()
-
-startinitpostop = start.getBoundingClientRect().top
-
-    start.addEventListener('mousemove', e => {
-        mouseFollower(e)
-});
+const startinitpostop = start.getBoundingClientRect().top
+const endbox = document.getElementById("end").getBoundingClientRect()
 
 console.log(boundary1.getBoundingClientRect().bottom)
 console.log(start.getBoundingClientRect().top)
 
+function starttask(){
+  start.addEventListener('mousemove', mouseFollower);
+};
+starttask()
+
 function mouseFollower(e) {
+
     var startpos = start.getBoundingClientRect()
+
     if ((startpos.top < boundary1pos.bottom && startpos.left < boundary1pos.right )
         || (startpos.top < boundary2pos.bottom)
         || (startpos.right > boundary3pos.left && startpos.top < boundary3pos.bottom)
         || (startpos.bottom > boundary4pos.top)
         || (startpos.right > boundary5pos.left && startpos.bottom > boundary5pos.top && startpos.left < boundary5pos.right)){
         console.log("You Lost")
-        document.removeEventListener('mousemove', lost());
-        start.style.left = 0 + 'px'
-        start.style.top = startinitpostop - boundary1pos.top + 'px'
+        start.removeEventListener('mousemove', mouseFollower);
+        lost();
+    }
+
+    else if(startpos.right>=endbox.right){
+      console.log("win!")
     }
     else{
         let left = e.clientX;
@@ -37,9 +42,20 @@ function mouseFollower(e) {
 }
 
 function lost(){
-    alert("Lost!")
+    start.style.left = 0 + 'px'
+    start.style.top = startinitpostop - boundary1pos.top + 'px'
+    alert("Lost!");
+    document.body.style.backgroundColor = "red";
+    retry()
 }
 
-function won(){
+function retry(){
+    start.addEventListener(('mouseover'), function(){
+        document.body.style.backgroundColor = "white";
+    })
+    start.addEventListener('click', starttask)
+}
 
+function win(){
+    pass;
 }
